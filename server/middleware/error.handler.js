37ba90dc48719cc1b5f0d4263217ea2a6fee3cbc
KIMENYI Kevin <kimenyikevin@gmail.com>
2019-08-23 17:22:56 +0100

    feature(user-signin): add user sign in on restful api [finishes: #168074849]

---
+++ server/middleware/error.handler.js
@@ -0,0 +1,9 @@
+const errorHandler = (err, req, res, next) => {
+    res.status(500)
+      .send({
+        status: 500,
+        error: err.message,
+      });
+  };
+  
+  export default errorHandler;
