1574248d0ae93a16b89af973fb6d64e3df836999
KIMENYI Kevin <kimenyikevin@gmail.com>
2019-08-24 11:26:54 +0100

    feature(change-user): admin change user to a mentor [start: #168083925]

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,8 +1,34 @@
-import express from 'express';
-import validate from '../middleware/SchemaValidator';
-import User from '../controllers/usersController';
-const { signup, signIn} = User;
+import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import validate from "../middleware/SchemaValidator";
+import User from "../controllers/usersController";
+import changeUser from "../controllers/changeUSer";
+import verify from "../middleware/admin";
+dotenv.config();
+const { signup, signIn } = User;
 const router = express.Router();
-router.post('/signup', validate.uservalidation, signup);
-router.post('/signin', signIn)
+router.post("/signup", validate.uservalidation, signup);
+router.post("/signin", signIn);
+router.patch("/user/:id", verify.verifyToken, changeUser.changeStatus);
+
+//Admin login information
+router.post("/user/admin", (req, res) => {
+  const user = {
+    id: 1,
+    email: "admin@gmail.com",
+    password: "admin"
+  };
+  jwt.sign(
+    { user },
+    process.env.SECRET_KEY,
+    { expiresIn: "1h" },
+    (err, token) => {
+      res.json({
+        token
+      });
+    }
+  );
+});
+
 export default router;
--- server/server.js
+++ server/server.js
@@ -11,4 +11,5 @@ const port = process.env.PORT || 3000;
 app.listen(port);
 
 app.use("/api/v1/auth", users);
+
 console.log("app running on port ", port);
