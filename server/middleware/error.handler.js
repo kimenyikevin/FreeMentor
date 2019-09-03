9edbf7d49ddfa593959212922b37cf5da6a85cdd
KIMENYI Kevin <kimenyikevin@gmail.com>
2019-09-03 11:30:26 +0100

    chore(delete apis): remove apis on gh-pages [finishes: #168251653]

--- server/middleware/error.handler.js
+++
@@ -1,9 +0,0 @@
- const errorHandler = (err, req, res, next) => {
-    res.status(500)
-      .send({
-        status: 500,
-        error: err.message,
-      });
-  };
-  
-  export default errorHandler;
