2ebf352929eed22158bf3fd2d88972af1d235d58
KIMENYI Kevin <kimenyikevin@gmail.com>
2019-09-06 10:22:29 +0100

    chore(error handle): route error handle
    [finishes: #168330214]

--- server/server.js
+++ server/server.js
@@ -1,23 +1,30 @@
 // server.js
-import express from "express";
-import users from "./routes/usersRoutes";
-import mentors from "./routes/mentorsroutes";
-import sessions from "./routes/sessionsRoutes";
-import errorHandler from "./middleware/error.handler";
+import express from 'express';
 import swaggerui from 'swagger-ui-express';
+import users from './routes/usersRoutes';
+import mentors from './routes/mentorsroutes';
+import sessions from './routes/sessionsRoutes';
+import errorHandler from './middleware/error.handler';
 import swaggerdocs from './swagger.json';
+
 const app = express();
 app.use(express.json());
 app.use('/api-docs', swaggerui.serve, swaggerui.setup(swaggerdocs));
 app.use(errorHandler);
-// eslint-disable-next-line no-undef
 const port = process.env.PORT || 3000;
 app.listen(port);
 
-app.use("/api/v1/auth", users);
-app.use("/api/v1/auth", mentors);
-app.use("/api/v1/auth", sessions);
+app.use('/api/v1/auth', users);
+app.use('/api/v1/auth', mentors);
+app.use('/api/v1/auth', sessions);
 
-console.log("app running on port ", port);
+app.use('/', (req, res) => {
+  res.status(405).send({
+    status: 405,
+    error: 'please specify method of router',
+  });
+});
+
+console.log('app running on port ', port);
 
 export default app;
